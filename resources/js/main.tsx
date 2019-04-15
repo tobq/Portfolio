2da4ffa1a76b1e7f96e8546de6830ff6d5b77072
tobi <tobi.akiny@gmail.com>
2019-04-16 00:26:45 +0100

    updated main scripts / css

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -1,2 +1,12 @@
 import "../sass/main.scss";
 import recording from "./recording";
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import TypeWriter from "./TypeWriter";
+
+const homeText = document.getElementById("home-text");
+const ref = React.createRef<TypeWriter>();
+ReactDOM.render(<TypeWriter recording={recording} ref={ref}/>, homeText);
+homeText.addEventListener("click", event => {
+    ref.current.run();
+});

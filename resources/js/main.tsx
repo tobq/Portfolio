2ca07e5fa3204dd6417a6e11e67b9d0105961317
tobi <tobi.akiny@gmail.com>
2019-04-16 03:14:06 +0100

    added video card. added todos

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -4,9 +4,13 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import TypeWriter from "./TypeWriter";
 import SKILLS from "./Skills";
+import cardData from "./cardData";
+import ImageCard from "./ImageCard";
+import VideoCard from "./VideoCard";
 
 const summary = document.getElementById("home-summary");
 const skills = document.getElementById("home-skills");
+const content = document.getElementById("content");
 
 const ref = React.createRef<TypeWriter>();
 ReactDOM.render(<TypeWriter recording={recording} ref={ref}/>, summary);
@@ -19,3 +23,14 @@ ReactDOM.render(SKILLS.map((skill, i) => {
     setTimeout(() => ref.current.classList.add("active"), i * 200);// Delays by 200ms
     return div
 }), skills);
+
+ReactDOM.render(cardData.map(cardProps => {
+    return "backgroundImage" in cardProps ?
+        <ImageCard {...cardProps}/> :
+        <VideoCard {...cardProps}/>
+}), content);
+
+
+// TODO: IMAGE URL ICON
+// TODO: PROJECTS LINK SCROLL DOWN
+// TODO: ADD MORE PROJECTS

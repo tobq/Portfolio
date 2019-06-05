a9653d5730c789330c6432a135521d9b986de3e6
tobi <tobi.akiny@gmail.com>
2019-06-05 20:44:08 +0100

    added TSS

--- resources/js/cardData.tsx
+++ resources/js/cardData.tsx
@@ -6,7 +6,7 @@ const cardData: (VideoCardProps | ImageCardProps)[] = [
     {
         url: "https://www.forestrock.co.uk/ev/",
         name: "Energy Vision",
-        date: "2018",
+        date: "Mar 2018",
         description: "I'm the lead developer on the Energy Vision product distributed by Forest Rock Ltd. It's a engineering / GUI tool use for Building Management Systems (BMS) to help evaluate resource usage across buildings. Technologies used vary through: Java, Gradle, GIT, JavaScript, ReactJS, Typescript and Tridium's Niagara Framework",
         backgroundImage: "url(image/ev.jpg)",
     },
@@ -39,6 +39,13 @@ const cardData: (VideoCardProps | ImageCardProps)[] = [
             <span id="note">This was later rectified, using automatic image manipulation, based on the rectangular bounds of the pool table.</span>
         </div>,
         src: "video/pool.mp4",
+    },
+    {
+        url: "https://www.instagram.com/thatsuccessshow",
+        name: "That Success Show",
+        date: "May 2016",
+        description: `I worked with PremierModa to create the website for their web-series: "That Success Show" - a show about luxury and success.`,
+        backgroundImage: "url(image/TSS.png)"
     },
     {
         url: "http://chalky.tobi.me.uk",

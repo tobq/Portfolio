9280b1ed5f693f42a763145c717a96ec49e55699
tobq <tobi.akiny@gmail.com>
2020-05-23 16:08:30 +0100

    tweaked nav bar - auto hide text on mobile

--- resources/js/recording.ts
+++ resources/js/recording.ts
@@ -1,61 +1,73 @@
-export default [{"offset": 569.5, "text": ""}, {"offset": 701.5, "text": ""}, {
-    "offset": 821.5,
+export default [{"offset": 0, "text": ""}, {"offset": 2998, "text": "Y"}, {
+    "offset": 3039,
     "text": "Y"
-}, {"offset": 865.5, "text": "Yo"}, {"offset": 877.5, "text": "You"}, {
-    "offset": 941.5,
-    "text": "Youn"
-}, {"offset": 1013.5, "text": "Young"}, {"offset": 1317.5, "text": "Young "}, {
-    "offset": 1405.5,
+}, {"offset": 3264, "text": "Young"}, {"offset": 3288, "text": "Young"}, {
+    "offset": 3324,
+    "text": "Young"
+}, {"offset": 3362, "text": "Young"}, {"offset": 3504, "text": "Young "}, {
+    "offset": 3724,
     "text": "Young l"
-}, {"offset": 1433.5, "text": "Young lo"}, {"offset": 1505.5, "text": "Young lon"}, {
-    "offset": 1573.5,
-    "text": "Young lond"
-}, {"offset": 1601.5, "text": "Young londo"}, {"offset": 1957.5, "text": "Young london"}, {
-    "offset": 2069.5,
-    "text": "Young london "
-}, {"offset": 2137.5, "text": "Young london b"}, {"offset": 2257.5, "text": "Young london ba"}, {
-    "offset": 2333.5,
-    "text": "Young london b"
-}, {"offset": 2413.5, "text": "Young london "}, {"offset": 2437.5, "text": "Young london"}, {
-    "offset": 2553.5,
+}, {"offset": 3923, "text": "Young lon"}, {"offset": 3974, "text": "Young lond"}, {
+    "offset": 4054,
+    "text": "Young londo"
+}, {"offset": 4159, "text": "Young london"}, {"offset": 4219, "text": "Young london"}, {
+    "offset": 4406,
     "text": "Young london-"
-}, {"offset": 2589.5, "text": "Young london-b"}, {"offset": 2617.5, "text": "Young london-ba"}, {
-    "offset": 2689.5,
+}, {"offset": 4597, "text": "Young london-bas"}, {"offset": 4649, "text": "Young london-bas"}, {
+    "offset": 4694,
     "text": "Young london-bas"
-}, {"offset": 2769.5, "text": "Young london-base"}, {"offset": 2789.5, "text": "Young london-based"}, {
-    "offset": 2861.5,
+}, {"offset": 4822, "text": "Young london-base"}, {"offset": 5071, "text": "Young london-based"}, {
+    "offset": 5421,
     "text": "Young london-based "
-}, {"offset": 2905.5, "text": "Young london-based f"}, {
-    "offset": 2993.5,
+}, {"offset": 5596, "text": "Young london-based fu"}, {
+    "offset": 5665,
     "text": "Young london-based fu"
-}, {"offset": 3045.5, "text": "Young london-based ful"}, {
-    "offset": 3109.5,
+}, {"offset": 5833, "text": "Young london-based ful"}, {
+    "offset": 5983,
     "text": "Young london-based full"
-}, {"offset": 3353.5, "text": "Young london-based fulls"}, {
-    "offset": 3385.5,
-    "text": "Young london-based full"
-}, {"offset": 3441.5, "text": "Young london-based full-"}, {
-    "offset": 3473.5,
-    "text": "Young london-based full-s"
-}, {"offset": 3545.5, "text": "Young london-based full-st"}, {
-    "offset": 3601.5,
+}, {"offset": 6207, "text": "Young london-based full-s"}, {
+    "offset": 6312,
+    "text": "Young london-based full-st"
+}, {"offset": 6382, "text": "Young london-based full-st"}, {
+    "offset": 6578,
     "text": "Young london-based full-sta"
-}, {"offset": 3661.5, "text": "Young london-based full-stac"}, {
-    "offset": 3717.5,
-    "text": "Young london-based full-stack"
-}, {"offset": 3801.5, "text": "Young london-based full-stack "}, {
-    "offset": 3845.5,
-    "text": "Young london-based full-stack d"
-}, {"offset": 3917.5, "text": "Young london-based full-stack de"}, {
-    "offset": 3949.5,
-    "text": "Young london-based full-stack dev"
-}, {"offset": 3977.5, "text": "Young london-based full-stack deve"}, {
-    "offset": 4061.5,
-    "text": "Young london-based full-stack devel"
-}, {"offset": 4085.5, "text": "Young london-based full-stack develo"}, {
-    "offset": 4117.5,
-    "text": "Young london-based full-stack develop"
-}, {"offset": 4129.5, "text": "Young london-based full-stack develope"}, {
-    "offset": 4589.5,
-    "text": "Young london-based full-stack developer"
-}, {"offset": 5089.5, "text": "Young london-based full-stack developer."}]
+}, {"offset": 6763, "text": "Young london-based full-st"}, {
+    "offset": 6925,
+    "text": "Young london-based full-s"
+}, {"offset": 7045, "text": "Young london-based full-"}, {
+    "offset": 7178,
+    "text": "Young london-based full"
+}, {"offset": 7358, "text": "Young london-based ful"}, {
+    "offset": 7543,
+    "text": "Young london-based fu"
+}, {"offset": 7685, "text": "Young london-based f"}, {"offset": 7869, "text": "Young london-based S"}, {
+    "offset": 7985,
+    "text": "Young london-based S"
+}, {"offset": 8030, "text": "Young london-based S"}, {
+    "offset": 8205,
+    "text": "Young london-based Sof"
+}, {"offset": 8285, "text": "Young london-based Sof"}, {
+    "offset": 8434,
+    "text": "Young london-based Softw"
+}, {"offset": 8494, "text": "Young london-based Softw"}, {
+    "offset": 8656,
+    "text": "Young london-based Softwa"
+}, {"offset": 8806, "text": "Young london-based Software"}, {
+    "offset": 8871,
+    "text": "Young london-based Software"
+}, {"offset": 9057, "text": "Young london-based Software "}, {
+    "offset": 9442,
+    "text": "Young london-based Software E"
+}, {"offset": 9447, "text": "Young london-based Software E"}, {
+    "offset": 9671,
+    "text": "Young london-based Software Eng"
+}, {"offset": 9741, "text": "Young london-based Software Eng"}, {
+    "offset": 10068,
+    "text": "Young london-based Software Engine"
+}, {"offset": 10127, "text": "Young london-based Software Engine"}, {
+    "offset": 10154,
+    "text": "Young london-based Software Engine"
+}, {"offset": 10329, "text": "Young london-based Software Engineer"}, {
+    "offset": 10403,
+    "text": "Young london-based Software Engineer"
+}, {"offset": 10804, "text": "Young london-based Software Engineer."}]

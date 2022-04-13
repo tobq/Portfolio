61b3199b60eba123cf84fb70fb7030672357085a
Tobi Akinyemi <tobi@akinyemi.org>
2022-04-13 17:06:23 +0100

    formatted typewriter recording

--- resources/js/recording.ts
+++ resources/js/recording.ts
@@ -1,1 +1,58 @@
-export default [{"offset":0,"text":"Y"},{"offset":32,"text":"Y"},{"offset":352,"text":"You"},{"offset":384,"text":"Youn"},{"offset":448,"text":"Youn"},{"offset":513,"text":"Young"},{"offset":736,"text":"Young "},{"offset":1057,"text":"Young L"},{"offset":1057,"text":"Young L"},{"offset":1312,"text":"Young Lon"},{"offset":1344,"text":"Young Lond"},{"offset":1408,"text":"Young Lond"},{"offset":1504,"text":"Young London"},{"offset":1528,"text":"Young London"},{"offset":1720,"text":"Young London-"},{"offset":1944,"text":"Young London-b"},{"offset":2008,"text":"Young London-bas"},{"offset":2040,"text":"Young London-bas"},{"offset":2232,"text":"Young London-base"},{"offset":2360,"text":"Young London-based"},{"offset":2488,"text":"Young London-based "},{"offset":2713,"text":"Young London-based S"},{"offset":2713,"text":"Young London-based S"},{"offset":2968,"text":"Young London-based Sf"},{"offset":3120,"text":"Young London-based Sfo"},{"offset":3376,"text":"Young London-based Sf"},{"offset":3536,"text":"Young London-based So"},{"offset":3600,"text":"Young London-based Sof"},{"offset":3601,"text":"Young London-based Sof"},{"offset":3792,"text":"Young London-based Soft"},{"offset":3888,"text":"Young London-based Softw"},{"offset":4080,"text":"Young London-based Softwa"},{"offset":4304,"text":"Young London-based Software"},{"offset":4336,"text":"Young London-based Software"},{"offset":4496,"text":"Young London-based Software "},{"offset":4688,"text":"Young London-based Software E"},{"offset":4689,"text":"Young London-based Software E"},{"offset":4904,"text":"Young London-based Software En"},{"offset":5000,"text":"Young London-based Software Eng"},{"offset":5160,"text":"Young London-based Software Engin"},{"offset":5224,"text":"Young London-based Software Engine"},{"offset":5288,"text":"Young London-based Software Engine"},{"offset":5448,"text":"Young London-based Software Engineer"},{"offset":5512,"text":"Young London-based Software Engineer"},{"offset":5912,"text":"Young London-based Software Engineer."}];
+export default [{
+    "offset": 0,
+    "text": "Y"
+}, {"offset": 32, "text": "Y"}, {"offset": 352, "text": "You"}, {
+    "offset": 384,
+    "text": "Youn"
+}, {"offset": 448, "text": "Youn"}, {"offset": 513, "text": "Young"}, {
+    "offset": 736,
+    "text": "Young "
+}, {"offset": 1057, "text": "Young L"}, {"offset": 1057, "text": "Young L"}, {
+    "offset": 1312,
+    "text": "Young Lon"
+}, {"offset": 1344, "text": "Young Lond"}, {"offset": 1408, "text": "Young Lond"}, {
+    "offset": 1504,
+    "text": "Young London"
+}, {"offset": 1528, "text": "Young London"}, {"offset": 1720, "text": "Young London-"}, {
+    "offset": 1944,
+    "text": "Young London-b"
+}, {"offset": 2008, "text": "Young London-bas"}, {"offset": 2040, "text": "Young London-bas"}, {
+    "offset": 2232,
+    "text": "Young London-base"
+}, {"offset": 2360, "text": "Young London-based"}, {"offset": 2488, "text": "Young London-based "}, {
+    "offset": 2713,
+    "text": "Young London-based S"
+}, {"offset": 2713, "text": "Young London-based S"}, {"offset": 2968, "text": "Young London-based Sf"}, {
+    "offset": 3120,
+    "text": "Young London-based Sfo"
+}, {"offset": 3376, "text": "Young London-based Sf"}, {
+    "offset": 3536,
+    "text": "Young London-based So"
+}, {"offset": 3600, "text": "Young London-based Sof"}, {
+    "offset": 3601,
+    "text": "Young London-based Sof"
+}, {"offset": 3792, "text": "Young London-based Soft"}, {
+    "offset": 3888,
+    "text": "Young London-based Softw"
+}, {"offset": 4080, "text": "Young London-based Softwa"}, {
+    "offset": 4304,
+    "text": "Young London-based Software"
+}, {"offset": 4336, "text": "Young London-based Software"}, {
+    "offset": 4496,
+    "text": "Young London-based Software "
+}, {"offset": 4688, "text": "Young London-based Software E"}, {
+    "offset": 4689,
+    "text": "Young London-based Software E"
+}, {"offset": 4904, "text": "Young London-based Software En"}, {
+    "offset": 5000,
+    "text": "Young London-based Software Eng"
+}, {"offset": 5160, "text": "Young London-based Software Engin"}, {
+    "offset": 5224,
+    "text": "Young London-based Software Engine"
+}, {"offset": 5288, "text": "Young London-based Software Engine"}, {
+    "offset": 5448,
+    "text": "Young London-based Software Engineer"
+}, {"offset": 5512, "text": "Young London-based Software Engineer"}, {
+    "offset": 5912,
+    "text": "Young London-based Software Engineer."
+}];
